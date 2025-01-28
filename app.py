d6ac47ae3f050e189bcb90023e5f304efb608197
BhavanaPanchangam <bhavanapanchangam@gmail.com>
2025-01-27 20:53:22 -0800

    requiremnts

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def filter_data():
 
     return render_template("dashboard.html", asteroids=filtered)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
